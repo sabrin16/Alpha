f035155761042c80ab3798a429cad05e0792d331
Sabrin16 <sabrin.salem@hotmail.se>
2025-03-14 02:00:06 +0100

    Add project files.

---
+++ Webapp/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webapp.Controllers;
+
+public class AuthController : Controller
+{
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+}
---
+++ Webapp/Controllers/ProjectsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webapp.Controllers;
+
+public class ProjectsController : Controller
+{
+    [Route("projects")]
+
+    public IActionResult Projects()
+    {
+        return View();
+    }
+}
