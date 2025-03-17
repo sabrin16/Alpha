c9c1998df85f1caeb11e7b1e7ea66e6168e9de33
Sabrin16 <sabrin.salem@hotmail.se>
2025-03-17 03:29:46 +0100

    Lagt till databaskontext och fixat kodstruktur

--- Webapp/Controllers/AdminController.cs
+++ Webapp/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Webapp.Controllers;
 
@@ -11,6 +12,33 @@ public class AdminController : Controller
 
     public IActionResult Members()
     {
+        return View();
+    }
+
+    [Route("clients")]
+
+    public IActionResult Clients()
+    {
+        return View();
+    }
+
+    [HttpPost]
+
+    public IActionResult AddClient(AddClientForm form)
+    {
+        if (!ModelState.IsValid)
+            return RedirectToAction("Client");
+
+        return View();
+    }
+
+    [HttpPost]
+
+    public IActionResult EditClient(AddClientForm form)
+    {
+        if (!ModelState.IsValid)
+            return RedirectToAction("Client");
+
         return View();
     }
 }
